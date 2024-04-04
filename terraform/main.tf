d7bbb657ba4c96dc09fd0bdd24d45bc89055888c
HARSHA2000 <patelharsha2000@gmail.com>
2024-04-04 15:31:16 +0530

    ran terraform fmt command for formatig

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "terraformdemo"
+    resource_group_name  = "terraformdemo"
     storage_account_name = "terraformstatecodeops"
-    container_name = "tfstatefile"
-    key = "dev.terraform.tfstate" 
+    container_name       = "tfstatefile"
+    key                  = "dev.terraform.tfstate"
   }
 }
 
 module "RG" {
-    source = "./modules/RG"
-    rgname = var.rgname
-    location = var.location 
+  source   = "./modules/RG"
+  rgname   = var.rgname
+  location = var.location
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "rgname" {
-    description = "Resource Group Name"
-    default = "terraformdemo1"
-    type = string
+  description = "Resource Group Name"
+  default     = "terraformdemo1"
+  type        = string
 }
 
 variable "location" {
-    description = "Azure location"
-    default = "Central India"
-    type = string
+  description = "Azure location"
+  default     = "Central India"
+  type        = string
 }
