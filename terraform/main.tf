8ef56b8e1ec897805e33e37c373207e9b79ffdfa
HARSHA2000 <patelharsha2000@gmail.com>
2024-04-04 15:18:09 +0530

    first commint with terraform files. This should trigger a git action.

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "terraformdemo"
+    storage_account_name = "terraformstatecodeops"
+    container_name = "tfstatefile"
+    key = "dev.terraform.tfstate" 
+  }
+}
+
+module "RG" {
+    source = "./modules/RG"
+    rgname = var.rgname
+    location = var.location
+  
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "rgname" {
+    description = "Resource Group Name"
+    default = "terraformdemo"
+    type = string
+}
+
+variable "location" {
+    description = "Azure location"
+    default = "Central India"
+    type = string
+}
